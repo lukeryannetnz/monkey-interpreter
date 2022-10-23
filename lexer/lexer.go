5fe72d62cbbb87617d956e4ab8b8900f39f7ce96
Luke Ryan <github@lukeryan.net.nz>
2022-10-23 14:00:14 +1300

    Lexer delimiter functionality complete, tests passing.

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -28,6 +28,16 @@ func (l *Lexer) NextToken() token.Token {
 		tok = token.New(token.PLUS, l.ch)
 	case '(':
 		tok = token.New(token.LPAREN, l.ch)
+	case ')':
+		tok = token.New(token.RPAREN, l.ch)
+	case '{':
+		tok = token.New(token.LBRACE, l.ch)
+	case '}':
+		tok = token.New(token.RBRACE, l.ch)
+	case ',':
+		tok = token.New(token.COMMA, l.ch)
+	case ';':
+		tok = token.New(token.SEMICOLON, l.ch)
 	}
 
 	l.readChar()
