47d53d35a79bb83fee131afc29aaad625c4e5efe
Luke Ryan <github@lukeryan.net.nz>
2022-10-30 13:55:20 +1300

    AST contains types for let statement.

--- ast/ast.go
+++ ast/ast.go
@@ -1,6 +1,8 @@
 // Package ast provides an abstract syntax tree to represent monkey source code.
 package ast
 
+import "monkey-interpreter/token"
+
 type Node interface {
 	TokenLiteral() string
 }
@@ -17,6 +19,7 @@ type Expression interface {
 	expressionNode()
 }
 
+// Program is the root of the AST which contains all other nodes
 type Program struct {
 	Statements []Statement
 }
@@ -28,3 +31,20 @@ func (p *Program) TokenLiteral() string {
 		return ""
 	}
 }
+
+type LetStatement struct {
+	Token token.Token
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode()       {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
