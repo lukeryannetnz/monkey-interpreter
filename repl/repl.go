1078e2aff62e186af34cbf9305592c57b9c995f4
Luke Ryan <github@lukeryan.net.nz>
2022-10-30 13:29:22 +1300

    Add basic repl.

---
+++ repl/repl.go
@@ -0,0 +1,31 @@
+// Package repl provides a read evaluate print loop for monkey source code.
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"monkey-interpreter/lexer"
+	"monkey-interpreter/token"
+)
+
+const PROMPT = ">> "
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		fmt.Fprintf(out, PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+
+		line := scanner.Text()
+		l := lexer.New(line)
+
+		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+			fmt.Fprintf(out, "%+v\n", tok)
+		}
+	}
+}
