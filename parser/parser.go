e59d4e94b0be9d2e53c77cd23326312663368b84
Luke Ryan <github@lukeryan.net.nz>
2022-10-30 14:03:34 +1300

    Scaffold parser.

---
+++ parser/parser.go
@@ -0,0 +1,38 @@
+// Package parser contains functionality syntactically analyse and convert tokens of the monkey programming language into the abstract syntax tree (AST) data structure.
+//
+// For more information on the AST, see the [ast] package.
+//
+// This parser uses a recursive descent approach, specifically it is a top down operator precedence or Pratt parser.
+package parser
+
+import (
+	"monkey-interpreter/ast"
+	"monkey-interpreter/lexer"
+	"monkey-interpreter/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	curToken  token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	// read two tokens so curToken and peekToken are populated
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgramme() *ast.Program {
+	return nil
+}
