8dcb5dd8dd611653341a84bfd06e38b7077bebcd
Luke Ryan <github@lukeryan.net.nz>
2022-11-13 21:01:51 +1300

    Check more of let statement

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -1,6 +1,7 @@
 package parser
 
 import (
+	"monkey-interpreter/ast"
 	"monkey-interpreter/lexer"
 	"testing"
 )
@@ -18,4 +19,49 @@ let foobar = 838383;
 	if program == nil {
 		t.Fatal("ParseProgram() returned nil")
 	}
+
+	if len(program.Statements) != 3 {
+		t.Fatalf("program.Statements does not contain 3 statements. got=%d", len(program.Statements))
+	}
+
+	tests := []struct {
+		expectedIdentifier string
+	}{
+		{"x"},
+		{"y"},
+		{"foobar"},
+	}
+
+	for i, tt := range tests {
+		stmt := program.Statements[i]
+		if !testLetStatement(t, stmt, tt.expectedIdentifier) {
+			return
+		}
+	}
+}
+
+func testLetStatement(t *testing.T, stmt ast.Statement, name string) bool {
+	if stmt.TokenLiteral() != "let" {
+		t.Errorf("s.TokenLiteral not 'let'. got=%q", stmt.TokenLiteral())
+		return false
+	}
+
+	letStmnt, ok := stmt.(*ast.LetStatement)
+
+	if !ok {
+		t.Errorf("stmt not *ast.LetStatement. got=%T", stmt)
+		return false
+	}
+
+	if letStmnt.Name.Value != name {
+		t.Errorf("letStmnt.Name.Vale not '%s'. got='%s", name, letStmnt.Name.Value)
+		return false
+	}
+
+	if letStmnt.Name.TokenLiteral() != name {
+		t.Errorf("letStmnt.Name.TokenLiteral() not '%s' got '%s'", name, letStmnt.Name.TokenLiteral())
+		return false
+	}
+
+	return true
 }
