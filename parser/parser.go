738104162f8c6b1e22aa35667491c397e69550c3
Luke Ryan <github@lukeryan.net.nz>
2023-01-07 13:42:38 +1300

    Adding error support to parser.

--- parser/parser.go
+++ parser/parser.go
@@ -15,10 +15,14 @@ type Parser struct {
 	l         *lexer.Lexer
 	curToken  token.Token
 	peekToken token.Token
+	errors    []string
 }
 
 func New(l *lexer.Lexer) *Parser {
-	p := &Parser{l: l}
+	p := &Parser{
+		l:      l,
+		errors: []string{},
+	}
 
 	// read two tokens so curToken and peekToken are populated
 	p.nextToken()
@@ -30,6 +34,10 @@ func New(l *lexer.Lexer) *Parser {
 func (p *Parser) nextToken() {
 	p.curToken = p.peekToken
 	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
 }
 
 func (p *Parser) ParseProgram() *ast.Program {
