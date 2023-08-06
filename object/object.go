303cca3e918fe222bbb352ff2c801c07fbf8d356
Luke Ryan <github@lukeryan.net.nz>
2023-08-06 19:27:30 +1200

    Add scaffold for object system.

---
+++ object/object.go
@@ -0,0 +1,32 @@
+// Package object contains the internal object system used when evaluating monkey source code.
+//
+// Every value in monkey is represented as a struct which implements an Object interface.
+package object
+
+import "fmt"
+
+type ObjectType string
+
+const (
+	INTEGER_OBJ = "INTEGER"
+	BOOLEAN_OBJ = "BOOLEAN"
+)
+
+type Object interface {
+	Type() ObjectType
+	Inspect() string
+}
+
+type Integer struct {
+	Value int64
+}
+
+func (i *Integer) Inspect() string  { return fmt.Sprintf("%d", i.Value) }
+func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
+
+type Boolean struct {
+	Value bool
+}
+
+func (b *Boolean) Inspect() string  { return fmt.Sprintf("%t", b.Value) }
+func (b *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
