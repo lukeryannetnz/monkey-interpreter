fe896e848a8280606a55d0080a843b887471eeb3
Luke Ryan <github@lukeryan.net.nz>
2024-02-22 13:51:47 +1300

    Fix bug in string Type function. Add failing string concat test.

--- object/object.go
+++ object/object.go
@@ -120,5 +120,5 @@ type String struct {
 	Value string
 }
 
-func (s *String) Type() ObjectType { return FUNCTION_OBJ }
+func (s *String) Type() ObjectType { return STRING_OBJ }
 func (s *String) Inspect() string  { return s.Value }
