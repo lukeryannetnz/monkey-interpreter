7cbac91a5b8b1f631455ff0471d7fc1b74e11e2b
Luke Ryan <github@lukeryan.net.nz>
2024-06-10 12:01:56 +1200

    Add support for array index expressions

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -69,6 +69,16 @@ func Eval(node ast.Node, environment *object.Environment) object.Object {
 			}
 			return Eval(node.Alternative, environment)
 		}
+	case *ast.IndexExpression:
+		left := Eval(node.Left, environment)
+		if isError(left) {
+			return left
+		}
+		index := Eval(node.Index, environment)
+		if isError(index) {
+			return index
+		}
+		return evalIndexExpression(left, index)
 	case *ast.Identifier:
 		return evalIdentifier(node.Value, environment)
 	case *ast.FunctionLiteral:
@@ -310,6 +320,27 @@ func evalExpressions(expressions []ast.Expression, env *object.Environment) []ob
 	}
 
 	return result
+}
+
+func evalIndexExpression(left, index object.Object) object.Object {
+	switch {
+	case left.Type() == object.ARRAY_OBJ && index.Type() == object.INTEGER_OBJ:
+		return evalArrayIndexExpression(left, index)
+	default:
+		return newError("index operator not supported: %s", left.Type())
+	}
+}
+
+func evalArrayIndexExpression(array, index object.Object) object.Object {
+	arrayObject := array.(*object.Array)
+	idx := index.(*object.Integer).Value
+	max := int64(len(arrayObject.Elements) - 1)
+
+	if idx < 0 || idx > max {
+		return NULL
+	}
+
+	return arrayObject.Elements[idx]
 }
 
 func extendFunctionEnv(
